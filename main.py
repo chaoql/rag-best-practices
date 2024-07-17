2e088a7288cded454382fcc83e592cdac5c71b81
chaoql <1415331985@qq.com>
2024-07-17 17:51:42 +0800

    RAG with Sliding-windows-chunking

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import Settings
-from llama_index.legacy.embeddings import FastEmbedEmbedding
 from llama_index.llms.ollama import Ollama
 from llama_index.core import SimpleDirectoryReader
-from llama_index.core.ingestion import IngestionPipeline
-from llama_index.core.node_parser import TokenTextSplitter
 from llama_index.core import VectorStoreIndex
 import qdrant_client
 from llama_index.vector_stores.qdrant import QdrantVectorStore
-from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import StorageContext
-from llama_index.core import PromptTemplate
-from tqdm.asyncio import tqdm
-import nest_asyncio
-from llama_index.legacy.retrievers.bm25_retriever import BM25Retriever
-import asyncio
-import os
 import warnings
 from llama_index.core.node_parser import SentenceWindowNodeParser
+from llama_index.core.postprocessor import MetadataReplacementPostProcessor
 
-# node_parser = SentenceWindowNodeParser.from_defaults(
-#     # how many sentences on either side to capture
-#     window_size=3,
-#     # the metadata key that holds the window of surrounding sentences
-#     window_metadata_key="window",
-#     # the metadata key that holds the original sentence
-#     original_text_metadata_key="original_sentence",
-# )
-# warnings.filterwarnings('ignore')
-
+warnings.filterwarnings('ignore')
 
 # 加载嵌入模型
 Settings.embed_model = HuggingFaceEmbedding(
@@ -49,28 +31,52 @@ vector_store = QdrantVectorStore(client=client, collection_name="paul_graham")
 # load data
 documents = SimpleDirectoryReader("./data").load_data()
 
-# Transformations
-pipeline = IngestionPipeline(transformations=[TokenTextSplitter(chunk_size=512), node_parser])
-
-# Extract nodes from documents
-nodes = pipeline.run(documents=documents)
+# Sliding windows chunking & Extract nodes from documents
+node_parser = SentenceWindowNodeParser.from_defaults(
+    # how many sentences on either side to capture
+    window_size=3,
+    # the metadata key that holds the window of surrounding sentences
+    window_metadata_key="window",
+    # the metadata key that holds the original sentence
+    original_text_metadata_key="original_sentence",
+)
+nodes = node_parser.get_nodes_from_documents(documents, show_progress=False)
 
 # indexing
 try:
-    storage_context = StorageContext.from_defaults(vector_store=vector_store, persist_dir="storeQ_test")
+    storage_context = StorageContext.from_defaults(vector_store=vector_store, persist_dir="storeQ")
     index = VectorStoreIndex(nodes=nodes, storage_context=storage_context)
 except:
     index = VectorStoreIndex(nodes=nodes)
-    index.storage_context.persist(persist_dir="storeQ_test")
+    index.storage_context.persist(persist_dir="storeQ")
 
 # prompt
-query_str = "什么是裸金属？"
+query_str = "How old is the boy's mother?"
+
+# replace the sentence in each node with its surrounding context.
+query_engine = index.as_query_engine(similarity_top_k=2,
+                                     # the target key defaults to `window` to match the node_parser's default
+                                     node_postprocessors=[
+                                         MetadataReplacementPostProcessor(target_metadata_key="window")
+                                     ],
+                                     )
 
 # 响应
-# response = Settings.llm.complete(query_str)
-# print(response)
-
-query_engine = index.as_query_engine(similarity_top_k=3)
 response = query_engine.query(query_str)
+window = response.source_nodes[0].node.metadata["window"]  # 长度为3的窗口，包含了文本两侧的上下文。
+sentence = response.source_nodes[0].node.metadata["original_sentence"]  # 检索到的文本
 
-print(str(response))
+print(f"Response: {str(response)}")
+print("------------------")
+print(f"Window: {window}")
+print("------------------")
+print(f"Original Sentence: {sentence}")
+
+"""
+Response: The boy's mother is described as having "thirty outside" which likely refers to her age. However, without further context or clarification from the text, it is impossible to determine exactly how old she is. The sentence suggests that she has been working hard and may be tired, but this does not necessarily imply a specific age.
+------------------
+Window: She had removed her dark glasses, and her eyebrows were clear, but her lips were too thin, and the lipstick was not rich enough.  If she stood up from the canvas recliner, she would look thin, perhaps the lines of her silhouette were too hard, like the strokes of a square-tipped fountain pen.  She looked twenty-five or twenty-six years old, but the age of a new school woman is like the age of an old-fashioned woman's wedding invitation, which requires what the expert scientist calls extrinsic evidence to determine its authenticity, and which cannot be seen by itself.  The boy's mother has thirty outside, wearing a half old black cheongsam, full of labor and tiredness, coupled with the natural upside down eyebrows, the more sad and pathetic.  The child is less than two years old, collapsed nose, two slits in the eyes, eyebrows high above, and eyes far away from each other to suffer from lovesickness, like the Chinese face in the newspaper caricature.  He had just begun to walk, and was constantly running about; his mother held a leash on him, and pulled him back when he could not run more than three or four steps.  His mother, who was afraid of the heat, was tired of pulling him, but she was also concerned about her husband's success down there and couldn't stop scolding the boy for being a nuisance. 
+------------------
+Original Sentence: The boy's mother has thirty outside, wearing a half old black cheongsam, full of labor and tiredness, coupled with the natural upside down eyebrows, the more sad and pathetic.
+
+"""
